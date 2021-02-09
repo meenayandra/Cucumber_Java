c3d04f318cd94baaf05ad9ef42ac3363ae14dd6e
meenayandra <yandrameena@gmail.com>
2021-02-08 19:36:00 -0800

    changetoTestng

--- Cucumber_Selenium/src/main/java/StepDefinition/Steps.java
+++ Cucumber_Selenium/src/main/java/StepDefinition/Steps.java
@@ -3,12 +3,14 @@ package StepDefinition;
 
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;		
-import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 import cucumber.api.java.en.Given;		
 import cucumber.api.java.en.Then;		
 import cucumber.api.java.en.When;		
 
+@Test
 public class Steps {				
 
     WebDriver driver;			
@@ -19,19 +21,22 @@ public class Steps {
        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");					
        driver= new FirefoxDriver();					
        driver.manage().window().maximize();			
-       driver.get("http://demo.guru99.com/v4");					
+       driver.get("http://demo.guru99.com/v4");	
+       System.out.println("Opened firefox");
     }		
 
     @When("^Enter the Username User(\\d+)and Password password(\\d+)$")	
     public void enter_the_Username_and_Password(String username,String password) throws Throwable 							
     {		
        driver.findElement(By.name("uid")).sendKeys(username);					
-       driver.findElement(By.name("password")).sendKeys(password);					
+       driver.findElement(By.name("password")).sendKeys(password);	
+       System.out.println("Entered user and pwd");
     }		
 
     @Then("^Reset the credential$")					
     public void	Reset_the_credential() throws Throwable 							
     {		
-       driver.findElement(By.name("btnReset")).click();					
+       driver.findElement(By.name("btnReset")).click();	
+       System.out.println("The values entered and clicked on reset");
     }		
 }		
